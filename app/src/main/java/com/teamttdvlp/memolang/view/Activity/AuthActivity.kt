d971425a355f695de859faec313f356b9c74baf3
MinTin <tiendvlp@gmail.com>
2019-07-24 18:47:56 +0700

    initial

---
+++ app/src/main/java/com/teamttdvlp/memolang/view/Activity/AuthActivity.kt
@@ -0,0 +1,13 @@
+package com.teamttdvlp.memolang.view.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.teamttdvlp.memolang.R
+
+class AuthActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
